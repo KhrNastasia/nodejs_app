50725d3a0f3b2ac577573a411a867a40f70b0273
KhrNastasia <khr.nastasia@gmail.com>
2025-02-12 17:43:48 +0300

    connect to bd

--- react-app/model/conect.js
+++ react-app/model/conect.js
@@ -1,15 +1,16 @@
   const mysql = require("mysql");
   const dbConfig = require("../config/db.config");
-  
-  // создаем соединение с нашей базой данных
+
   const connection = mysql.createConnection({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+    database: dbConfig.DB,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
   
-  // открываем наше соединение с базой данных
   connection.connect(err => {
     if (err) {
       console.log('что-то')
@@ -23,4 +24,3 @@
   });
 
   module.exports = connection;
-   //экспортируем наше соединение
--- server.js
+++ server.js
@@ -3,15 +3,21 @@ process.on('uncaughtException', function (err) {
   });
 
 const express = require('express');
-// const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-// const log = require('./libs/log')(module);
+const log = require('./libs/log')(module);
 const app = express();
+const Sequelize = require('sequelize');
+const { error } = require('winston');
+const dbConfig = require("./react-app/config/db.config");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: 'mssql'
+});
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-// app.use(express.logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.logger('dev'));
 
 app.get( '/express_backend', (req, res) => {
     res.send({ message: "Это стартовая страница нашего приложения" });
@@ -38,19 +44,39 @@ app.delete('/api/articles/:id', function (req, res){
     res.send('This is not implemented now');
 });
 
-app.listen(port, ()=>console.log(`Listening on port ${port}`))
+app.listen(port, async()=>{
+    try{
+        await sequelize.authenticate();
+        console.log(`Connection has been established successful`)
+        console.log(`Listening on port ${port}`)
+    } catch{
+        console.error('Unable to connect to the database:', error)
+    }
+})
 
-// app.use((req, res, next)=>{
-//     res.status(404);
-//     log.debug('Not found URL: %s',req.url);
-//     res.send({ error: 'Not found' });
-//     return;
-// });
+app.use(function(req, res, next){
+    res.status(404);
+    log.debug;
+    // log.debug('Not found URL: %s',req.url);
+    res.send({ error: 'Not found',
+        st: log.debug
+    });
+    return;
+});
 
-// app.use((err, req, res, next)=>{
-//     res.status(500);
-//     // log.debug('Internal error(%d): %s',res.statusCode,err.message);
-//     res.send({ error: res.statusCode,
-//         typeof: err.message });
-//     return;
-// });
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    ;
+    log.error('Internal error(%d): %s',res.statusCode,err.message);
+    res.send({ 
+        error: err.message,
+        st: log.error
+    });
+    return;
+});
+
+
+app.get('/ErrorExample', function(req, res, next){
+    next(new Error('Random error!'));
+});
